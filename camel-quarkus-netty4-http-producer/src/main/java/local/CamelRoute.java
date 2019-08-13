cbea4d59bdb58f2995012030b9a2f09030b85104
hannes.holtzhausen@9thbit.co.za <hannes.holtzhausen@9thbit.co.za>
2019-08-13 15:41:09 +0200

    netty4-http producer RejectedExecutionException test.

---
+++ camel-quarkus-netty4-http-producer/src/main/java/local/CamelRoute.java
@@ -0,0 +1,19 @@
+package local;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+
+@ApplicationScoped
+public class CamelRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("servlet://hello?matchOnUriPrefix=true")
+                .setBody(constant("HelloWorld"));
+
+        from("netty4-http:http://0.0.0.0:8999/greeter")
+          .to("netty4-http:http://localhost:8080/greeter/hello");
+    }
+
+}
